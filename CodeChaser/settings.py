be58e54482a0dcfeaedf49cd3f6f93cd2502a0ac
ShouravAhmed <ahmed15-898@diu.edu.bd>
2023-07-11 23:12:52 +0600

    deploy

--- CodeChaser/settings.py
+++ CodeChaser/settings.py
@@ -156,7 +156,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # --------------------------------------------
 # celery
 # --------------------------------------------
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = env('CELERY_BROKER_URL')
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
